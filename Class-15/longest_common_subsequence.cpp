83e47aee61061819d143b7c16255493a6933c1d0
Pranay Bansal <44213415+PranayB200@users.noreply.github.com>
2022-02-26 14:25:08 +0530

    class 15

--- Class-15/longest_common_subsequence.cpp
+++ Class-15/longest_common_subsequence.cpp
@@ -34,8 +34,39 @@ int LCS(string s1, string s2) {
     return LCS_helper(s1, s2, 0, 0, memo);
 }
 
+
+int LCS_bottom_up(string s1, string s2) {
+
+    int n = s1.length(), m = s2.length();
+    vector<vector<int>> dp(n + 1, vector<int> (m + 1));
+
+    // Filling the dp table for the base-cases of recursion
+    for (int j = 0; j <= m; j++) {
+        dp[n][j] = 0;
+    }
+    for (int i = 0; i <= n; i++) {
+        dp[i][m] = 0;
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = m - 1; j >= 0; j--) {
+            if (s1[i] == s2[j]) {
+                dp[i][j] = 1 + dp[i + 1][j + 1];
+            }
+            else {
+                dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
+            }
+        }
+    }
+
+    return dp[0][0];
+}
+
 int main() {
 
     cout << LCS("ABCDGH", "AEDFHR") << endl;
     cout << LCS("ABC", "AC") << endl;
+
+    cout << LCS_bottom_up("ABCDGH", "AEDFHR") << endl;
+    cout << LCS_bottom_up("ABC", "AC") << endl;
 }
